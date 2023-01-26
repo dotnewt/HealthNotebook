9070f1ca932fbff2a2efc69b282e4b499b7f076b
dotnewt <Antithesis@DESKTOP-E7CE1C0>
2023-01-26 14:47:32 +0500

    Add project files.

---
+++ HealthNotebook.Api/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthNotebook.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("TestRun")]
+        public IActionResult TestRun()
+        {
+            return Ok("success");
+        }
+    }
+}
